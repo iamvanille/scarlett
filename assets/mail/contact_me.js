9dc4a57935399fb50496779a27ce554dfdb10102
Vanille Debray <vanille.paitier-debray@mail.mcgill.ca>
2021-03-26 12:33:34 -0400

    Made presentable version to show Scarlett

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong>Votre message a été envoyé. </strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Désolé " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", le serveur courriel ne répond pas, réessayez plus tard!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
